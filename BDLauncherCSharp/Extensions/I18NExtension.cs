3d51e02c8c231c9432a1c5f3b42050d4431758fc
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-11-09 22:21:02 +0800

    本地化方法扩充

--- BDLauncherCSharp/Extensions/I18NExtension.cs
+++ BDLauncherCSharp/Extensions/I18NExtension.cs
@@ -9,11 +9,18 @@ using System.Windows;
 
 namespace BDLauncherCSharp.Extensions
 {
+    /// <summary>
+    /// 全球化扩展
+    /// </summary>
     public static class I18NExtension
     {
         private static IEnumerable<string> names;
         private static ResourceManager resourceManager;
 
+        /// <summary>
+        /// 对应用进行初始化
+        /// </summary>
+        /// <param name="resourceMgr">资源管理</param>
         public static void I18NInitialize(this ResourceManager resourceMgr)
         {
             if (resourceMgr is null) throw new NullReferenceException($"this {nameof(ResourceManager)} is null");
@@ -23,38 +30,62 @@ namespace BDLauncherCSharp.Extensions
                                ?.Cast<DictionaryEntry>()
                                 .Select(item => item.Key as string);
         }
+
+        /// <summary>
+        /// 对控件进行初始化
+        /// </summary>
+        /// <param name="element">控件</param>
         public static void I18NInitialize(this DependencyObject element)
         {
-            if (!(element is UIElement target)) return;
-
+            if (element is null) return;
             // 获取子元素
             foreach (var child in LogicalTreeHelper.GetChildren(element))
                 (child as DependencyObject).I18NInitialize();
 
-            if (string.IsNullOrWhiteSpace(target.Uid)) return;
-
-            foreach (var (name, property)
-                in names.Select(name => (name, prefx: $"{target.Uid}."))
-                                      .Where(i => i.name?.StartsWith(i.prefx) ?? false)
-                                      .Select(i => (i.name, i.name.Substring(i.prefx.Length))))
+            if (element is UIElement target)
             {
-                var propertyInfo = element.GetType().GetProperty(property);
-                var str = resourceManager.GetString(name);
-                if (propertyInfo is null || string.IsNullOrWhiteSpace(str)) continue;
-                var constructor = propertyInfo.PropertyType.GetConstructor(new[] { typeof(string) });
-                var method = propertyInfo.PropertyType.GetMethod("Parse", BindingFlags.Static | BindingFlags.Public,
-                                                             null, new[] { typeof(string) }, null);
+                if (string.IsNullOrWhiteSpace(target.Uid)) return;
 
-                propertyInfo.SetValue(element, propertyInfo.PropertyType == typeof(string)
-                                               ? str
-                                               : constructor is null
-                                                   ? method is null
-                                                         ? str
-                                                         : method.Invoke(null, new object[] { str })
-                                                   : constructor.Invoke(new object[] { str }));
+                foreach (var (name, property)
+                    in names.Select(name => (name, prefx: $"{target.Uid}."))
+                                          .Where(i => i.name?.StartsWith(i.prefx) ?? false)
+                                          .Select(i => (i.name, i.name.Substring(i.prefx.Length))))
+                {
+                    var propertyInfo = element.GetType().GetProperty(property);
+
+                    var i18nStr = I18N(name);
+
+                    if (propertyInfo is null || string.IsNullOrWhiteSpace(i18nStr)) continue;
+
+
+                    if (propertyInfo.PropertyType == typeof(string))
+                        propertyInfo.SetValue(element, i18nStr);
+                    else
+                    {
+                        var constructor = propertyInfo.PropertyType.GetConstructor(new[] { typeof(string) });
+                        if (constructor is ConstructorInfo)
+                            propertyInfo.SetValue(element, constructor.Invoke(new object[] { i18nStr }));
+                        else
+                        {
+                            var method = propertyInfo.PropertyType.GetMethod("Parse",
+                                BindingFlags.Static | BindingFlags.Public,
+                                null, new[] { typeof(string) }, null);
+
+                            if (method is MethodInfo)
+                                propertyInfo.SetValue(element, method.Invoke(null, new object[] { i18nStr }));
+                            else
+                                propertyInfo.SetValue(element, i18nStr);
+                        }
+                    }
+                }
             }
         }
 
+        /// <summary>
+        /// 获取对应的本地化资源
+        /// </summary>
+        /// <param name="key">资源键</param>
+        /// <returns>本地化字符串</returns>
         public static string I18N(this string key) => resourceManager.GetString(key);
     }
 }
