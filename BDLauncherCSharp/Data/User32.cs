45664cf62868988e38fcfd3ad9344b788faa74e6
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-12-03 22:00:16 +0800

    拆分类

---
+++ BDLauncherCSharp/Data/User32.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using BDLauncherCSharp.Extensions;
+
+namespace BDLauncherCSharp.Data
+{
+    public class User32
+    {
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumDisplaySettings(string deviceName, int modeNum, out DEVMODE devMode);
+
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int index);
+    }
+}
